9a9a63ddfc90d904fe51a2c74a2621134167945b
Mehedi <mehedifreelancer452@gmail.com>
2026-03-06 13:00:36 +0600

    --

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,9 +34,7 @@ export default function RootLayout({
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-logo-purple-soft dark:bg-logo-purple/5 rounded-full blur-3xl opacity-10"></div>
               </div>
               {/* Content */}
-              <div className="relative z-10">
-                {children}
-              </div>
+              <div className="relative z-10">{children}</div>
             </main>
             <Footer />
           </ThemeProvider>
